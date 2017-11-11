61d6acc76de71728569b68bacd6b7448193f30eb
Andrei Reinus <andrei.reinus@gmail.com>
2017-11-11 23:33:09 +0200

    Admin rest API

---
+++ src/Vault.UI.AdminRestApi/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vault.Core;
+using Vault.Core.Entities;
+
+namespace Vault.UI.AdminRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class BookController : Controller
+    {
+        private readonly ICrudInteractor<Book> _interactor;
+
+        public BookController(ICrudInteractor<Book> interactor)
+        {
+            _interactor = interactor;
+        }
+
+        [Route("")]
+        public async Task<Book[]> GetAll()
+        {
+            var items = await _interactor.GetAllAsync();
+
+            return items.Entity;
+        }
+
+    }
+}
