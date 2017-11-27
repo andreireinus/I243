dcd8803f3dcbc7e8ce826822922a521f58b2aa36
Andrei Reinus <andrei.reinus@gmail.com>
2017-11-27 20:48:02 +0200

    Cleanup

--- src/Vault.UI.AdminForm/swagger-codegen.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-rd /q /s ClientApi
-
-autorest --input-file=http://localhost:55451/swagger/v1/swagger.json --output-folder=ClientApi --enable-xml=false --csharp --sync-methods=none --client-side-validation=false --payload-flattening-threshold=1 --openapi-type=arm --namespace=Vault.UI.AdminForm.ClientApi
